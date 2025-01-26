e11114797e364e186e9b06e534f4c746755b2b7e
imransbaigcpa <38595295-imransbaigcpa@users.noreply.replit.com>
2025-01-26 01:59:38 +0000

    Assistant checkpoint: Configure Gunicorn for production deployment
    Assistant generated file changes:
    - gunicorn.conf.py: Configure Gunicorn for production
    
    ---
    
    User prompt:
    
    Try to validate location at: '/github/workspace'.
    App Directory Location: '/' was found.
    Try to validate location at: '/github/workspace/swa-db-connections'.
    Looking for event info
    Starting to build app with Oryx
    Azure Static Web Apps utilizes Oryx to build both static applications and Azure Functions. You can find more details on Oryx here: https://github.com/microsoft/Oryx
    ---Oryx build logs---
    
    Operation performed by Microsoft Oryx, https://github.com/Microsoft/Oryx
    You can report issues at https://github.com/Microsoft/Oryx/issues
    
    Oryx Version: 0.2.20241003.1, Commit: d5352431d933306ccee1be9b5d822c73bf723e9e, ReleaseTagName: 20241003.1
    
    Build Operation ID: 6f63124b274ae17c
    OS Type           : bullseye
    Image Type        : githubactions
    
    Detecting platforms...
    Detected following platforms:
      nodejs: 18.20.4
    Version '18.20.4' of platform 'nodejs' is not installed. Generating script to install it...
    Error: Could not find either 'build' or 'build:azure' node under 'scripts' in package.json. Could not find value for custom run build command using the environment variable key 'RUN_BUILD_COMMAND'.Could not find tools for building monorepos, no 'lerna.json' or 'lage.config.js' files found.
    
    ---End of Oryx build logs---
    Oryx could not find a 'build' or 'build:azure' script in the package configuration. Please add one of these commands to your package configuration file (i.e. package.json). Alternatively, you can add the app_build_command to the build/deploy section of your workflow file. For example, app_build_command: 'npm run docs:build'
    
    For further information, please visit the Azure Static Web Apps documentation at https://docs.microsoft.com/en-us/azure/static-web-apps/
    If you believe this behavior is unexpected, please raise a GitHub issue at https://github.com/azure/static-web-apps/issues/
    Exiting

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -6,7 +6,7 @@ backlog = 2048
 
 # Worker processes
 workers = 4
-worker_class = 'sync'
+worker_class = "sync"
 worker_connections = 1000
 timeout = 120
 keepalive = 2
@@ -15,7 +15,7 @@ keepalive = 2
 accesslog = "-"
 access_log_format = '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"'
 errorlog = "-"
-loglevel = 'info'
+loglevel = "info"
 
 # Process naming
 proc_name = 'gunicorn_fastclose'
